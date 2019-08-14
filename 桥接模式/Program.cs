dd7742f7c654f448ed437e27329e99f77ec2c957
Jie Huang <jehg@mm-software.com>
2019-08-14 11:19:23 +0800

    add Adapter pattern.

---
+++ 桥接模式/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace 桥接模式
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ 适配器模式/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Text;
+
+namespace 适配器模式
+{
+    /// <summary>
+    /// 可以使用的接口
+    /// </summary>
+    public interface Target
+    {
+        void GetTemperature();
+
+        void GetPressure();
+
+        void GetHumidity();
+
+        void GetUltraviolet();
+    }
+
+    public class Adaptee
+    {
+        public void 得到温度()
+        {
+            Console.WriteLine("得到了温度");
+        }
+
+        public void 得到气压()
+        {
+            Console.WriteLine("得到了气压");
+        }
+
+        public void 得到湿度()
+        {
+            Console.WriteLine("得到了湿度");
+        }
+
+        public void 得到紫外线()
+        {
+            Console.WriteLine("得到了紫外线");
+        }
+    }
+
+    public class Adapter : Adaptee, Target
+    {
+        public void GetTemperature()
+        {
+            得到温度();
+        }
+
+        public void GetPressure()
+        {
+            得到气压();
+        }
+
+        public void GetHumidity()
+        {
+            得到湿度();
+        }
+
+        public void GetUltraviolet()
+        {
+            得到紫外线();
+        }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var adapter = new Adapter();
+            adapter.GetTemperature();
+            adapter.GetHumidity();
+            adapter.GetPressure();
+            adapter.GetUltraviolet();
+        }
+    }
+}
