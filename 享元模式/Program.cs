33ae9392b87a314cdb698704d800eea873fdb6fc
narojay <hj200812@126.com>
2019-08-19 15:05:26 +0800

    Add flyweight pattern.

---
+++ 享元模式/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace 享元模式
+{
+
+    //共享对象
+    public abstract class AbstractEnemy
+    {
+        public string Color { set; get; }
+
+        public int x;
+        public int y;
+
+        public AbstractEnemy()
+        {
+            Color = "红色";
+        }
+
+        public override string ToString()
+        {
+            return $"generate {Color} Enemy ,located in ({x},{y})";
+        }
+    }
+
+    public class Enemy : AbstractEnemy
+    {
+        public Enemy(int y)
+        {
+            this.y = y;
+        }
+    }
+
+    public class FactoryEnemy
+    {
+        private readonly Hashtable  _enemyList;
+
+        public FactoryEnemy()
+        {
+            _enemyList = new Hashtable {{1, new Enemy(100)}, {2, new Enemy(200)}};
+
+        }
+
+        public Enemy GetEnemy(int num)
+        {
+            return _enemyList[num] as Enemy;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factoryEnemy = new FactoryEnemy();
+            var enemy = factoryEnemy.GetEnemy(1);
+            Console.WriteLine(enemy);
+            var enemy1 = factoryEnemy.GetEnemy(2);
+            Console.WriteLine(enemy1);
+        }
+    }
+}
---
+++ 代理模式/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace 代理模式
+{
+    p
+
+    /// <summary>
+    /// 代理模式就是给一个对象提供代理，代理对象起到客户端和目标对象之前的中介作用 比如桌面快捷方式就是一个代理
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
