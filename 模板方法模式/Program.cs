837c5bddbddbf81777f88558c5890dde66f48d19
narojay <hj200812@126.com>
2020-03-25 17:29:00 +0800

    add 模板方法

---
+++ 模板方法模式/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace 模板方法模式
+{
+    public abstract class Recipe
+    {
+
+        public void Cook()
+        {
+            AddOil();
+            AddVegatable();
+            AddWater();
+        }
+        public void AddOil()
+        {
+            Console.WriteLine("add oil");
+        }
+
+        public void AddWater()
+        {
+            Console.WriteLine("add water");
+        }
+
+        public abstract void AddVegatable();
+    }
+
+
+    public class TomatoRecipe : Recipe
+    {
+        public override void AddVegatable()
+        {
+            Console.WriteLine("add tomato");
+        }
+    }
+    public class PotatoRecipe : Recipe
+    {
+        public override void AddVegatable()
+        {
+            Console.WriteLine("add Potato");
+        }
+    }
+
+    public class Client
+    {
+        public static void Main(string[] args)
+        {
+            Recipe recipe1 = new PotatoRecipe();
+            recipe1.Cook();
+            Console.WriteLine("====================================");
+            Recipe recipe2 = new TomatoRecipe();
+            recipe2.Cook();
+
+        }
+      
+
+
+    }
+}
