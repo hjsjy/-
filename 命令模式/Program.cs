f7bc94c18ac5f2b28f16484468698133f602b032
narojay <hj200812@126.com>
2020-03-25 17:28:27 +0800

    add 命令模式

---
+++ 命令模式/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace 命令模式
+{
+    public class Recevier
+    {
+        public void Run()
+        {
+            Console.WriteLine("run ");
+        }
+    }
+
+    public abstract class Command
+    {
+        protected readonly Recevier _recevier;
+
+        public Command(Recevier recevier)
+        {
+            _recevier = recevier;
+        }
+
+        public abstract void Action();
+    }
+
+    public class ConcreteCommand : Command
+    {
+        public ConcreteCommand(Recevier recevier) : base(recevier)
+        {
+
+        }
+
+        public override void Action()
+        {
+            _recevier.Run();
+        }
+    }
+
+    public class Invoker
+    {
+        private readonly Command _command;
+
+        public Invoker(Command command)
+        {
+            _command = command;
+        }
+
+        public void Excute()
+        {
+            _command.Action();
+        }
+    }
+
+    public class Client
+    {
+        public static void Main(string[] args)
+        {
+            var command = new ConcreteCommand(new Recevier());
+            var invoker = new Invoker(command);
+            invoker.Excute();
+        }
+    }
+}
